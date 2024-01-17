13ca8e4b823ea7b0066faf611718a42668c7d2bf
Nik Startsev <nikstar1235@gmail.com>
2024-01-17 12:15:43 +0300

    2. IndexController. Таблица и вид. [#2092 #472566]

--- src/main/java/ru/job4j/accidents/repository/AccidentMemRepository.java
+++ src/main/java/ru/job4j/accidents/repository/AccidentMemRepository.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Repository;
 import ru.job4j.accidents.model.Accident;
-
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
