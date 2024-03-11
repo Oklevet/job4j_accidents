4c538c59b46b6faf8e4387221de00a303f114c82
Nik Startsev <nikstar1235@gmail.com>
2024-03-11 12:38:15 +0300

    0. Spring Security [#6879]

---
+++ src/main/java/ru/job4j/accidents/auth/SecurityInit.java
@@ -0,0 +1,6 @@
+package ru.job4j.accidents.auth;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
